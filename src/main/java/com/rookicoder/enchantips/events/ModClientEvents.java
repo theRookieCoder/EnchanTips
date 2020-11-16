3acaf7f65add4be58d75dd20f4c98fcbd75a6885
Ilesh <ileshkt@gmail.com>
2020-11-16 08:20:13 +0800

    Added the rest of the enchantments in Minecraft.

--- src/main/java/com/rookicoder/enchantips/events/ModClientEvents.java
+++ src/main/java/com/rookicoder/enchantips/events/ModClientEvents.java
@@ -137,11 +137,128 @@ public class ModClientEvents {
                             "\nWorks with swords." +
                             "\nUp to level III"));
 
-        } else if (enchants.containsKey(Enchantments.INFINITY) && Screen.hasShiftDown()) {
+        } else if (enchants.containsKey(Enchantments.LOYALTY) && Screen.hasShiftDown()) {
             event.getToolTip().add(new StringTextComponent(
                     "Returns trident after thrown. Higher level decreases return time" +
                             "\nWorks with tridents." +
                             "\nIncompatible with Riptide." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.LUCK_OF_THE_SEA) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases rate of good loot." +
+                            "\nWorks with fishing rods." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.LURE) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Decreases wait time until fish/junk/loot \'bites\'." +
+                            "\nWorks with fishing rods." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.MENDING) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Repair item while gaining xp drops." +
+                            "\nWorks with everything enchantable." +
+                            "\nIncompatible with Infinty" +
+                            "\nUp to level I"));
+
+        } else if (enchants.containsKey(Enchantments.MULTISHOT) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Shoot 3 arrows at the cost of 1." +
+                            "\nWorks with crossbows." +
+                            "\nIncompatible with Piercing" +
+                            "\nUp to level I"));
+
+        } else if (enchants.containsKey(Enchantments.POWER) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases arrow damage." +
+                            "\nWorks with bows." +
+                            "\nUp to level V"));
+
+        } else if (enchants.containsKey(Enchantments.PROJECTILE_PROTECTION) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Reduces projectile damage." +
+                            "\nWorks with all armour and turtle shells." +
+                            "\nIncompatible with Blast, Fire and Normal protection." +
+                            "\nUp to level IV"));
+
+        } else if (enchants.containsKey(Enchantments.PROTECTION) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Reduces most typed of damage by 4% for each level." +
+                            "\nWorks with all armour and turtle shells." +
+                            "\nIncompatible with Projectile, Fire and Blast protection" +
+                            "\nUp to level IV"));
+
+        } else if (enchants.containsKey(Enchantments.PUNCH) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases arrow knockback." +
+                            "\nWorks with bows." +
+                            "\nUp to level II"));
+
+        } else if (enchants.containsKey(Enchantments.QUICK_CHARGE) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Decreases crossbow charging time." +
+                            "\nWorks with crossbows." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.RESPIRATION) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Extends underwater breathing time." +
+                            "\nWorks with all helmets and turtle shells." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.RIPTIDE) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Launches player itself when thrown. Only works in the water or rain. " +
+                            "\nWorks with tridents." +
+                            "\nIncompatible with Channeling and Loyalty" +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.SHARPNESS) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases damage." +
+                            "\nWorks with swords and axes." +
+                            "\nIncompatible with Bane of Arthropods and Smite" +
+                            "\nUp to level V"));
+
+        } else if (enchants.containsKey(Enchantments.SILK_TOUCH) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Mined block drop themselves." +
+                            "\nWorks with all pickaxes, axes, shovels, hoes, and shears." +
+                            "\nIncompatible with Fortune" +
+                            "\nUp to level I"));
+
+        } else if (enchants.containsKey(Enchantments.SMITE) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases damage to undead mobs." +
+                            "\nWorks with swords and axes." +
+                            "\nIncompatible with Bane of Arthropods and Sharpness" +
+                            "\nUp to level V"));
+
+        } else if (enchants.containsKey(Enchantments.SOUL_SPEED) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases walking speed on soul sand and soul soil." +
+                            "\nWorks with all boots." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.SWEEPING) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases sweeping attack damage." +
+                            "\nWorks with all swords." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.THORNS) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Reflects damage taken when hit at the cost of durability." +
+                            "\nWorks with all armour and turtle shells." +
+                            "\nUp to level III"));
+
+        } else if (enchants.containsKey(Enchantments.UNBREAKING) && Screen.hasShiftDown()) {
+            event.getToolTip().add(new StringTextComponent(
+                    "Increases item durability." +
+                            "\nWorks with everything enchantable." +
+                            "\nCOMPATIBLE with Mending" +
                             "\nUp to level III"));
 
         }
